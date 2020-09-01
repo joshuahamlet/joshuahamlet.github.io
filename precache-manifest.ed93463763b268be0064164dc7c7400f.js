acf2b0bcf4cd23c46c1379855e7b95c9dae18ca7
Joshua Hamlet <joshua7hamlet@gmail.com>
2020-09-01 16:09:21 -0700

    Updates

---
+++ precache-manifest.ed93463763b268be0064164dc7c7400f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0e8a318ca4a70937409f5430b000d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "797583ec2df895f54a54",
+    "url": "/static/css/main.e8b277d3.chunk.css"
+  },
+  {
+    "revision": "fbed01e8c9ceea0b4601",
+    "url": "/static/js/2.fb6a2835.chunk.js"
+  },
+  {
+    "revision": "5d2584cb7cadfdd428e2137214cdba4b",
+    "url": "/static/js/2.fb6a2835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "797583ec2df895f54a54",
+    "url": "/static/js/main.fa58b9e9.chunk.js"
+  },
+  {
+    "revision": "502809ca03967f587be8",
+    "url": "/static/js/runtime-main.e495b04f.js"
+  },
+  {
+    "revision": "87175716a375582a2339426aa94382b3",
+    "url": "/static/media/SquadaOne-Regular.87175716.ttf"
+  }
+]);
