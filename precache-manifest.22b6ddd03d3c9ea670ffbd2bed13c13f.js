0db535c0eab815b46df762af2c1864087593645d
Joshua Hamlet <Joshua7hamlet@gmail.com>
2023-02-07 12:56:53 -0800

    Updates

---
+++ precache-manifest.22b6ddd03d3c9ea670ffbd2bed13c13f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "647c465f6b62755c0e68bc12408bd048",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bdc3e0b0fd7bdf95a11",
+    "url": "/static/css/main.0e3fed79.chunk.css"
+  },
+  {
+    "revision": "62b59a1de313138d4795",
+    "url": "/static/js/2.f34de736.chunk.js"
+  },
+  {
+    "revision": "a015bc7b48827f451cc7117eff82a976",
+    "url": "/static/js/2.f34de736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bdc3e0b0fd7bdf95a11",
+    "url": "/static/js/main.fe364e2b.chunk.js"
+  },
+  {
+    "revision": "502809ca03967f587be8",
+    "url": "/static/js/runtime-main.e495b04f.js"
+  },
+  {
+    "revision": "87175716a375582a2339426aa94382b3",
+    "url": "/static/media/SquadaOne-Regular.87175716.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/joshuahamlet.github.io/precache-manifest.2cdf703bacffe3781471f48f2ce26d0b.js"
+  "/precache-manifest.22b6ddd03d3c9ea670ffbd2bed13c13f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/joshuahamlet.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
