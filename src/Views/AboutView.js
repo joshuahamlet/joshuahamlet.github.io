95127be04314547eebd876b2993c00d8caaac767
Joshua Hamlet <jhamlet@tuta.io>
2023-02-07 16:44:51 -0800

    dev: markdown pulling logic

--- src/Views/AboutView.js
+++ src/Views/AboutView.js
@@ -7,15 +7,14 @@ import './AboutView.css'
 const AboutView = () => {
 
   useEffect(()=> {
-  fetch('https://raw.githubusercontent.com/joshuahamlet/legend-of-sellda/master/README.md')
+  fetch('https://raw.githubusercontent.com/joshuahamlet/phaser-wario-like-platformer/main/README.md')
     .then(response => response.text())
     .then(text => {
       // console.log(text)
-      let pos1 = text.indexOf("###")
-      let pos2 = text.indexOf("###", (pos1 + 3));
-      return text.slice(pos1 + 4, pos2);
+      let pos1 = text.indexOf("<i>")
+      let pos2 = text.indexOf("</i>", (pos1 + 3));
+      return text.slice(pos1 + 3, pos2).trim();
       })
-
     .then(data => console.log(data))
   })
     return (
@@ -41,6 +40,13 @@ const AboutView = () => {
         </div>
         <div className='mini-card'>
         socials
+          <i>asdfasdfasdf
+            asdfasdfasdfasdf
+            asdfasdfasdfasdfasd
+            asdfsdfasdfs
+            asdfsffgsfd
+
+          </i>
         </div>
         </div>
         </motion.div>
