a7ea569b4ac8f7b10193f1867e63539a6a745020
Joshua Hamlet <jhamlet@tuta.io>
2023-02-13 16:38:52 -0800

    feat: icon updates

--- src/data.js
+++ src/data.js
@@ -2,28 +2,39 @@ import React from 'react'
 
 import { GiSpinningSword } from 'react-icons/gi'
 import { IoMdPizza } from 'react-icons/io'
+import { IoTerminal } from 'react-icons/io5'
 import { CgNotes } from 'react-icons/cg'
 import { AiOutlineDashboard } from 'react-icons/ai'
 
 export const projects = [
-    {
-        id: "L",
-        title: "Legend of Sellda",
-        icon: <GiSpinningSword/>,
-    },
-    {
-        id: "P",
-        title: "Pomodoro Pizza party",
-        icon: <IoMdPizza/>,
-    },
-    {
-        id: "T",
-        title: "TBD Project",
-        icon: <CgNotes/>,
-    },
-    {
-        id: "G",
-        title: "TBD Project",
-        icon: <AiOutlineDashboard/>,
-    }
+  {
+    id: 'L',
+    title: 'Legend of Sellda',
+    icon: <GiSpinningSword />,
+    link: 'https://legend-of-sellda.web.app',
+  },
+  {
+    id: 'P',
+    title: 'Pomodoro Pizza party',
+    icon: <IoMdPizza />,
+    link: 'https://wariolikeplatformer.web.app',
+  },
+  {
+    id: 'T',
+    title: 'TBD Project',
+    icon: <IoTerminal />,
+    link: 'https://react-txt-adventure.web.app',
+  },
+  {
+    id: 'G',
+    title: 'TBD Project',
+    icon: <AiOutlineDashboard />,
+    link: '',
+  },
+  {
+    id: 'D',
+    title: 'Next',
+    icon: <AiOutlineDashboard />,
+    link: '',
+  },
 ]
